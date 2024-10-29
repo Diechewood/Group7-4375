5c351c0d17929e0c5f955306d85e351ee7b339a3
johnphuongtran <123337709+johnphuongtran@users.noreply.github.com>
2024-10-29 13:47:27 -0500

    Update creds.py
    Updated the new endpoint in the creds.py file.

--- Backend/creds.py
+++ Backend/creds.py
@@ -1,5 +1,5 @@
 class Creds:
-    conString = 'frosted-fabrics.c14uy6mu0o87.us-east-1.rds.amazonaws.com'
+    conString = 'frosted-fabrics.ctesqau4gr0e.us-east-1.rds.amazonaws.com'
     userName ='firelink'
     password = 'fabric123'
     dbName = 'Frosted_Fabrics'
