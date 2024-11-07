29eb90c9a3b37edf1925ff6dcc9167b93e93a401
John Doe <elian721@gmail.com>
2024-11-06 19:13:58 -0600

    Fix lines

--- Backend/main.py
+++ Backend/main.py
@@ -9,7 +9,6 @@ import traceback
 from urllib.parse import unquote
 
 
-
 # Set up logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
