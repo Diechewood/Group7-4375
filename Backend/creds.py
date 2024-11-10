38785d87567c63e609c7fcd911444c90b05f7976
Kevin <kdtojin@cougarnet.uh.edu>
2024-11-10 17:11:51 -0600

    implementation of environment variables and docker support

--- Backend/creds.py
+++ Backend/creds.py
@@ -1,5 +1,11 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+print(os.getenv('dbusername'))
+
 class Creds:
     conString = 'frosted-fabrics.ctesqau4gr0e.us-east-1.rds.amazonaws.com'
-    userName ='firelink'
-    password = 'fabric123'
+    userName = os.getenv('dbusername')
+    password = os.getenv('dbpassword')
     dbName = 'frostedfabrics'
